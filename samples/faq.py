d72a73194c75d721387ca4bea588cc7e47de1463
Marcus Ottosson <marcus@abstractfactory.io>
2014-04-06 12:00:34 +0100

    Adding FAQ from correspondence with Justin Israel (thanks Justin!)

--- samples/faq.py
+++ samples/faq.py
@@ -21,11 +21,13 @@ if not os.path.exists(path):
     os.mkdir(path)
 
 """
-Q: Why store simple data in folders?
-A: To support the notion of programmable folders. Like with programming,
-    metadata changes often and storing it separately from where it is
-    consumed often leads to out-of-date information. Open Metadata allows
-    for an "eat your own dog-food" methodology.
+Q: Why store data in relation to folders?
+
+A: To support the notion of programmable folders. Like with documentation
+    and the code it documents, metadata changes often and storing it
+    separately from where it is consumed often leads to out-of-date
+    information. Open Metadata allows for an "eat your own dog-food"-
+    methodology in that metadata is not only stored but used as well.
 
 """
 
@@ -40,15 +42,17 @@ def why_simple_data():
 
 """
 Q: How do I concatenate paths?
+
 A: With Open Metadata nodes, there should be very little reason to have
     to manually concatenate paths. The reason this is is because Open
     Metadata could run on any platform, and won't necessarily be dealing
-    with paths on a file-system, they might be paths in a database.
+    with paths on a file-system; e.g. they might be paths in a database or
+    any arbitrary storage-model.
 """
 
 
 def no_concatenation():
-    location = om.read(path)
+    location = om.Location(path)
     firstchild = location.children.next()
 
     parent_dir = os.path.dirname(firstchild.path)
@@ -62,6 +66,92 @@ def no_concatenation():
 
 
 """
+Q: How do I store my lists and dicts (i.e. collection data-types)?
+
+A: There are exactly two methods of storing any data:
+    1. Blobs
+    2. Open Metadata-types
+
+"""
+
+
+def storing_collection_as_blobs():
+    """
+    Open Metadata can store any arbitrary data as blobs. Blobs are stored
+    together with its parent; i.e. in the simples of situation, blobs are
+    copied from their source into the Open Metadata hierarchy.
+
+     __________________       ____________________________
+    |                  |     |                            |
+    | /root/source.bin |---->| /location/.meta/source.bin |
+    |__________________|     |____________________________|
+
+    """
+
+    raise NotImplementedError
+
+    path = '/project/asset'
+    location = om.Location(path)
+    mydata = om.Blob('mydata', parent=location)
+    mydata.data = '/path/to/mydata.json'
+    om.dump(mydata)
+
+    # Retreiving stored blob
+    mydata = om.read(path, 'mydata')
+    mydata.path
+    # --> '/project/asset/.meta/mydata.json'
+
+    assert mydata.data == mydata.path
+
+
+def storing_collection_as_open_metadata():
+    """
+    While blobs facilitate storage of any arbitrary data, Open Metadata also
+    has the notion of Open Metadata-types.
+
+    Open Metadata-types offer extended features for plain-data, such as text,
+    numbers, dates and matrices in that they may be added, subtracted, version-
+    controlled and eventually end up in user interfaces as views and editors
+    relevant to their specific type.
+
+     _______________________       ___________________________________________
+    |                       |     |                                           |
+    | ['entry1', 'entry2' ] |---->| /location/.meta/mydata.list/entry1.string |
+    |_______________________|     |___________________________________________|
+                             .
+                             .     ___________________________________________
+                             .    |                                           |
+                             .--->| /location/.meta/mydata.list/entry2.string |
+                                  |___________________________________________|
+
+    The advantage of each ultimately depends on your use-case, but in short:
+
+    - If control > performance, use Open Metadata-types
+    - If control < performance, use Blobs
+
+    """
+
+    raise NotImplementedError
+
+    path = '/project/asset'
+    location = om.Location(path)
+    mydata = om.Group('mydata.list', parent=location)
+    mydata = ['entry1', 'entry2']
+    om.dump(mydata)
+
+    # Retreiving stored blob
+    mydata = om.read(path, 'mydata')
+    mydata.path
+    # --> '/project/asset/.meta/mydata.list'
+
+    mydata.data
+    # --> [String('entry1'), String('entry2')]
+
+    assert mydata.data != mydata.path
+
+
+"""
 Q: How do I traverse a hierarchy?
-A: 
+
+A: Hierarchies are 
 """
